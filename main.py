823219ff17e0c0909bae48137e0feaf63f244698
yaroslavpodolyaka-lang <yaroslav.podolyaka@student.sumdu.edu.ua>
2025-10-11 18:48:25 +0300

    show_by_group
    Подоляка Ярослав

--- main.py
+++ main.py
@@ -65,6 +65,7 @@ def show_all(data):
             print("Предмети та оцінки:")
             for subject, grade in info['subjects'].items():
                 print(f"   {subject}: {grade}")
+# Функція для перегляду студентів певної групи - виконав Подоляка Ярослав
 def show_by_group(data):
             group = input("Введіть назву групи: ")
             print(f"\nСтуденти групи {group}:")
